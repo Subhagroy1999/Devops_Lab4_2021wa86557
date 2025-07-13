9cac86d0c2c080886e30f9ece369bca9ecb29f7d
Subhagroy1999 <2021wa86557@wilp.bits-pilani.ac.in>
2025-07-13 11:59:44 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -12,11 +12,7 @@ public class App {
         System.out.println("🚀 CI Pipeline Demo");
 
         System.out.println("Hello from GitHub, Maven, and Jenkins!");
-
-        //Updatd message
         System.out.println("Build triggered at: " + LocalDateTime.now());
-
-        //New functionality or change
         system.out.println("This is an update to the source code!");
     }
 
