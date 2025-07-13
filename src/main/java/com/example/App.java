729e04735295f9fee25302a169764f8efcafbd6d
Subhagroy1999 <2021wa86557@wilp.bits-pilani.ac.in>
2025-07-13 11:51:33 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,21 +1,23 @@
-package com.example; 
+package com.example;
 
- 
 
-import java.time.LocalDateTime; 
+import java.time.LocalDateTime;
 
- 
 
-public class App { 
 
-    public static void main(String[] args) { 
+public class App {
 
-        System.out.println("🚀 CI Pipeline Demo"); 
+    public static void main(String[] args) {
 
-        System.out.println("Hello from GitHub, Maven, and Jenkins!"); 
+        System.out.println("🚀 CI Pipeline Demo");
 
-        System.out.println("Build triggered at: " + LocalDateTime.now()); 
+        System.out.println("Hello from GitHub, Maven, and Jenkins!");
 
-    } 
+        //Updatd message
+        System.out.println("Build triggered at: " + LocalDateTime.now());
+
+        //New functionality or change
+        system.out.println("This is an update to the source code!");
+    }
 
 }
